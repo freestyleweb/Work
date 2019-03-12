a17987cbf4428618ab6bc6aeb95815fe8222e13d
freestyleweb <47541930+freestyleweb@users.noreply.github.com>
2019-03-12 16:11:02 +0200

    addK9

---
+++ K9_Dnepr/app/js/common.js
@@ -0,0 +1,31 @@
+$(function() {
+
+	$('#my-menu').mmenu({
+        extensions: [ 'widescreen', 'theme-white', 'effect-menu-slide', 'pagedim-black'],
+        navbar: {
+            title: '<img src="../img/Logo.svg" alt="К9 Днепр">'
+        },
+        offCanvas: {
+            position: 'right'
+        }
+    })
+    
+	var api = $('#my-menu').data('mmenu');
+	api.bind('opened', function () {
+        $('.hamburger').addClass('is-active');
+    }).bind('closed', function () {
+        $('.hamburger').removeClass('is-active');
+    });
+
+	$('.carousel-main').owlCarousel({
+        loop: true,
+        nav: true,
+        items: 1,
+        smartSpeed: 700,
+        navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
+        responsiveClass: true,
+
+
+    });
+
+});
