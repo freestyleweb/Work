14da72f948d4fe2458e63e5a56701c9af274a21d
freestyleweb <47541930+freestyleweb@users.noreply.github.com>
2019-08-06 17:37:47 +0300

    add footer

--- Composite/app/js/main.js
+++ Composite/app/js/main.js
@@ -10,6 +10,36 @@ $(document).ready(function(){
       arrows : false,
       cssEase: 'linear'
     });
+
+    $('.video__slider').slick({
+      autoplay: false,
+      dots: false,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      arrows : true,
+      cssEase: 'linear'
+    });
+
+
+    $('.products__gallery').magnificPopup({
+      delegate: 'a',
+      type: 'image',
+      tLoading: 'Loading image #%curr%...',
+      mainClass: 'mfp-img-mobile',
+      gallery: {
+        enabled: true,
+        navigateByImgClick: true,
+        preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+      },
+      image: {
+        tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+        // titleSrc: function(item) {
+        //   return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+        // }
+      }
+    });
+
     
   });
 
