eca94d878b665a5123bf448707f71e4a23fb40a0
freestyleweb <47541930+freestyleweb@users.noreply.github.com>
2019-09-19 17:39:55 +0300

    add responsive

--- Glo_Expert/js/script.js
+++ Glo_Expert/js/script.js
@@ -1,8 +1,22 @@
 $(document).ready(function() {
 
+		var hamb_btn = $('.hamburger-btn');
+		var hamb_btn_active = $('.hamburger-btn_active');
+		var mobile_menu = $('.mobile-menu');
+		var nav_link = $('.mobile-menu li');
+	
+		hamb_btn.click(function(){
+			hamb_btn.toggleClass('hamburger-btn_active');
+			mobile_menu.toggleClass('mobile-menu_active');
+		});
+		nav_link.click(function(){
+			hamb_btn.toggleClass('hamburger-btn_active');
+			mobile_menu.toggleClass('mobile-menu_active');
+		});
+
 	// youtube-video in popup
 	$('.popup-youtube').magnificPopup({
-		disableOn: 700,
+		disableOn: 319,
 		type: 'iframe',
 		mainClass: 'mfp-fade',
 		removalDelay: 160,
@@ -10,10 +24,33 @@ $(document).ready(function() {
 
 		fixedContentPos: false
 	});
+
+
+	//button to top
+	var btn = $('#button');
+
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > 300) {
+      btn.addClass('show');
+    } else {
+      btn.removeClass('show');
+    }
+  });
+
+  btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, '300');
+  });
 	
 
 	//sticky menu
-	$("#sticker").sticky({topSpacing:0});
+	$('#sticker').sticky({topSpacing:0});
+
+
+	//burger button
+	$('.burger').on('click', function(e){
+		e.preventDefault;
+	});
 
 	
 	// sales countdown
