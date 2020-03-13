40d118f3c9f0df9ab208586d371638967e30838b
freestyleweb <elvud@ukr.net>
2020-03-13 17:25:09 +0200

    final desktop

--- Exclusive A&M/js/script.js
+++ Exclusive A&M/js/script.js
@@ -1,10 +1,18 @@
-$(function () {
+const header = document.querySelector('.header');
+const headerAfterSend = document.querySelector('.header-after-send');
+const headerSendButton = document.querySelector('#header__send-button');
+const inputName = document.querySelector('#inputName');
+const inputPhone = document.querySelector('#inputPhone');
 
-	$('.slider__items').slick({
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		dots: true,
-		fade: true,
-	});
 
-});
+headerSendButton.onclick = function (e) {
+
+	event.preventDefault();
+	if (inputName.value.length > 0 && inputPhone.value.length > 0) {
+		header.style.display = 'none';
+		headerAfterSend.style.display = 'block';
+	} else {
+		alert('Please, enter Your name and Your phone');
+	}
+
+}
