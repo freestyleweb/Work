faccead6da7926a16ce9e2868d23f323b05f7603
freestyleweb <elvud@ukr.net>
2020-03-16 16:47:28 +0200

    add mobile menu

--- Exclusive A&M/js/script.js
+++ Exclusive A&M/js/script.js
@@ -3,6 +3,9 @@ const headerAfterSend = document.querySelector('.header-after-send');
 const headerSendButton = document.querySelector('#header__send-button');
 const inputName = document.querySelector('#inputName');
 const inputPhone = document.querySelector('#inputPhone');
+const mobileMenu = document.querySelector('.mobile-menu');
+const burgerButton = document.querySelector('.burger__button');
+const burgerButtonClose = document.querySelector('.burger__button-close');
 
 
 headerSendButton.onclick = function (e) {
@@ -15,4 +18,18 @@ headerSendButton.onclick = function (e) {
 		alert('Please, enter Your name and Your phone');
 	}
 
-}
+};
+
+document.querySelector('.burger__button').addEventListener('click', function () {
+	mobileMenu.classList.toggle('active');
+	burgerButton.classList.toggle('hide');
+	burgerButtonClose.classList.toggle('active');
+
+});
+
+// document.burgerButton.onclick = function () {
+// 	document.querySelector('.mobileMenu').toggleClass('active');
+// };
+
+
+
