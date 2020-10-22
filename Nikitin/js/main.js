97c43269a23f31557882212ee636e37cbb0e4b4b
freestyleweb <elvud@ukr.net>
2020-10-22 15:00:33 +0300

    Nikitin start

---
+++ Nikitin/js/main.js
@@ -0,0 +1,14 @@
+// $(function() {
+
+//   });
+
+$('.main-slider').slick({
+  autoplay: true,
+  autoplaySpeed: 8000,
+  dots: true,
+  arrows: true,
+  infinite: true,
+  speed: 1000,
+  slidesToShow: 1,
+  adaptiveHeight: true
+});
