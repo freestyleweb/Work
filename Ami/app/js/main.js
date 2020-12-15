cf06f6b215c6695bd3652f56f4f4129871eaaba7
freestyleweb <elvud@ukr.net>
2020-12-15 18:10:14 +0200

    start ami project

---
+++ Ami/app/js/main.js
@@ -0,0 +1,29 @@
+// var mySwiper = new Swiper('.swiper-container', {
+// Optional parameters
+// direction: 'vertical',
+// loop: true,
+
+// If we need pagination
+// pagination: {
+// 	el: '.swiper-pagination',
+// },
+
+// Navigation arrows
+// navigation: {
+// 	nextEl: '.swiper-button-hidden',
+// 	prevEl: '.swiper-button-hidden',
+// },
+
+// And if we need scrollbar
+// scrollbar: {
+// 	el: '.swiper-scrollbar',
+// },
+// })
+
+new Swiper('.swiper-container', {
+	direction: 'horizontal',
+	loop: 'true',
+	pagination: {
+		el: '.swiper-pagination',
+	},
+});
